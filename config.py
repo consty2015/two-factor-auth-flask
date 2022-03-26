3a85b72d3b3e40b73e737b9b7e7cf79fe4b42b61
consty2015 <cngamelue@gmail.com>
2022-03-26 16:40:07 -0500

    modified Database

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 SECRET_KEY = 'top-secret'
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite')
+SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Welcome123@consdb.c2fba95zjgl1.us-east-1.rds.amazonaws.com/consDB"
+#os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
